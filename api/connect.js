02afbab7f680f5e3b887f287ddc2d24758f8e572
wanz-codename-similarity <xcvispport@gmail.com>
2025-11-11 17:31:42 +0700

    delete user wanz via Telegram bot

--- api/connect.js
+++ api/connect.js
@@ -43,19 +43,13 @@ const axiosOpt = {
 /* ====== IN-MEMORY USERS ====== */
 let users = [
   {
-    username: "wanz",
-    password: "p",
-    role: "mods",
-    createdAt: 1762856780180,
-    expired: 1762943180180
-  },
-  {
-    username: "ciwin",
-    password: "ciwin",
-    role: "Developer",
-    createdAt: 1762345091067,
-    expired: 1764937091067
+    "username": "ciwin",
+    "password": "ciwin",
+    "role": "Developer",
+    "createdAt": 1762345091067,
+    "expired": 1764937091067
   }
+
 ];
 
 /* ====== HELPERS ====== */
